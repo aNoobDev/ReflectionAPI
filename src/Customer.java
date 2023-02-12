af8e970a642d668c3e1619cbd3bfde35988e132c
Aftab Alam <alamaftab777@gmail.com>
2023-02-12 13:14:12 +0530

    Reflection API

--- src/Customer.java
+++ src/Customer.java
@@ -1,2 +1,40 @@
-package PACKAGE_NAME;public class Customer {
+import java.sql.SQLOutput;
+
+public class Customer {
+
+    private final String name;
+    private int age;
+
+    public Customer(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void walk(){
+        System.out.println("Walking......");
+    }
+
+    private void thisIsPrivateMethod(){
+        System.out.println("I am Private Method");
+    }
+
+    private static void thisIsPrivateStaticMethod(){
+        System.out.println("I am Private Static Method");
+    }
+    public static void thisIsPublicStaticMethod(){
+        System.out.println("I am Public Static Method");
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
         System.out.println("Hello world!");
+        Customer customer = new Customer("Michael", 19);
+        System.out.println(customer.getAge() + " " + customer.getName());
+        Field[] customerFields = customer.getClass().getDeclaredFields();
+
+        for (Field field : customerFields) {
+
+            if (field.getName().equals("name")) {
+                field.setAccessible(true);
+                field.set(customer, "David warner");
+            }
+        }
+        System.out.println(customer.getAge() + " " + customer.getName());
+
+        Method[] customerMethods = customer.getClass().getDeclaredMethods();
+        for (Method method : customerMethods) {
+            if (method.getName().equals("thisIsPublicStaticMethod")) {
+                //method.setAccessible(true);
+                method.invoke(null);
+            }
+            if (method.getName().equals("thisIsPrivateStaticMethod")) {
+                method.setAccessible(true);
+                method.invoke(null);
+            }
+            if (method.getName().equals("thisIsPrivateMethod")) {
+                method.setAccessible(true);
+                method.invoke(customer);
+            }
+        }
     }
 }
